4236d643bcd410350cfd08bfbffe0ca58b90665c
José Lucas <jose_lucas77@outlook.com>
2022-01-22 18:24:32 -0300

    update

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,8 @@
-var vel= 70
+var vel= 50
 console.log(`A velocidade do seu carro é de ${vel}Km/h`)
 if (vel > 60){
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
 console.log(`Diriga sempre usando o cinto de segurança!`)
+
+// para executar é só usar o "node .\ex008.js"
---
+++ treinando/teste.js
@@ -0,0 +1,2 @@
+var nome = prompt("Digite um nome: ")
+console.log(nome)
